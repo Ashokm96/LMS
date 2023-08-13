39ef4115ee92bad7f25ca12e8748d2292b229697
RelangiNagendra <108651514+RelangiNagendra@users.noreply.github.com>
2023-08-14 00:21:03 +0530

    code coverage set up

--- WebSvc/LMS.Api/LMS.API.Test/LMSTest.cs
+++ WebSvc/LMS.Api/LMS.API.Test/LMSTest.cs
@@ -6,12 +6,14 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LMS.API.Test
 {
+    [ExcludeFromCodeCoverage]
     public class LMSTest
     {
 
