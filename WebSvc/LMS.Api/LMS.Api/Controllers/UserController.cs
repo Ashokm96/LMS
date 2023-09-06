0dbb35fe08222b6e02378ab214b005a6d618efb7
RelangiNagendra <108651514+RelangiNagendra@users.noreply.github.com>
2023-09-07 00:11:44 +0530

    status code changes

--- WebSvc/LMS.Api/LMS.Api/Controllers/UserController.cs
+++ WebSvc/LMS.Api/LMS.Api/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using LMS.Api.Services.Contract;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace LMS.Api.Controllers
 {
@@ -24,46 +25,42 @@ namespace LMS.Api.Controllers
         /// <returns></returns>
         [HttpPost]
         [Route("/api/v1.0/lms/user/register")]
-        public ActionResult<Result<string>> Post([FromBody] Users users)
+        public ActionResult Post([FromBody] Users users)
         {
-            Result<string> result = new Result<string>();
             if (users == null)
             {
                 string message = string.Format("User details cannot be null.");
-                result.ErrorMessage = message;
-                return result;
+                return StatusCode((int)HttpStatusCode.BadRequest, message);
             }
             if (!users.Email.Contains("@") || !users.Email.Contains(".com"))
             {
-                return BadRequest("Invalid email format.");
+                return StatusCode((int)HttpStatusCode.BadRequest, "Invalid email format.");
             }
             if (!IsPasswordValid(users.Password))
             {
-                return BadRequest("Password should be alphanumeric and at least 8 characters.");
+                return StatusCode((int)HttpStatusCode.BadRequest, "Password should be alphanumeric and at least 8 characters.");
             }
             //validate user details
             var userDetails = userService.ValidateUser(users.Email,users.UserName);
             if (userDetails.Result == "invalidUser")
             {
-                return BadRequest("User details already exists.");
+                return StatusCode((int)HttpStatusCode.Conflict, "User details already exists.");
             }
             if (userDetails.Result == "error")
             {
-                return BadRequest("An error occured. contact your system administrator.");
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occured. contact your system administrator.");
             }
             //add new user
             userService.Register(users);
             if (users.UserID != null)
             {
-                result.Data = users.UserID;
-                result.Message = "Registered succefully";
-                return result;
+                return StatusCode((int)HttpStatusCode.Created, "Registered succefully");
             }
             if (users.UserID == null)
             {
-                return BadRequest("Failed to register");
+                return StatusCode((int)HttpStatusCode.Conflict, "Failed to register");
             }
-            return result;
+            return StatusCode((int)HttpStatusCode.InternalServerError, "An error occured. contact your system administrator.");
         }
 
         private bool IsPasswordValid(string password)
@@ -84,14 +81,14 @@ namespace LMS.Api.Controllers
         [HttpPost]
         [Route("/api/v1.0/lms/user/login")]
         [AllowAnonymous]
-        public ActionResult<string> Login(Login login)
+        public ActionResult Login(Login login)
         {
             var token = userService.Authenticate(login.Username, login.Password);
             if (token == null)
             {
-                return BadRequest("unauthorized user");
+                return StatusCode((int)HttpStatusCode.NotFound, "user details not found.");
             }
-            return Ok(new { Token = token });
+            return StatusCode((int)HttpStatusCode.Created, new { Token = token });
         }
 
         #endregion
