9352111d5c51474f347e59fdfb916c30def41b52
RelangiNagendra <108651514+RelangiNagendra@users.noreply.github.com>
2023-09-21 16:07:09 +0530

    auth set up

---
+++ WebUI/LMS-UI/src/app/services/auth.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { login } from '../models/login';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  public login(loginCredntials:login) :Observable<any> {
+    let url: string = "";
+    return this.http.post(url,loginCredntials);
+  }
+
+  isLoggedIn(): boolean {
+    //check if user is logged in
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() :void {
+    localStorage.removeItem('token');
+  }
+
+  hasAnyRole(role:String[]):boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.decodeToken(token);
+      const userRole = decodedToken.role;
+      return role.includes(userRole);
+    }
+    return false;
+  }
+
+  isAdmin(): boolean {
+    // Check if the user has the 'Admin' role
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.decodeToken(token);
+      const userRole = decodedToken.role;
+      return userRole === 'Admin';
+    }
+    return false;
+  }
+  private decodeToken(token: string): any {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (error) {
+      return null;
+    }
+  }
+
+}
