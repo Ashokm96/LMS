164431d7995b514351668d8e80af96841c3af084
RelangiNagendra <108651514+RelangiNagendra@users.noreply.github.com>
2023-09-27 17:07:29 +0530

    search functionality added

--- WebUI/LMS-UI/src/app/Components/home/home.component.ts
+++ WebUI/LMS-UI/src/app/Components/home/home.component.ts
@@ -1,176 +1,210 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { ToastService } from '../../Common/toast.service';
-import { AddCoursePopupComponent } from '../../dialogues/add-course-popup/add-course-popup.component';
-import { DeleteCoursePopupComponent } from '../../dialogues/delete-course-popup/delete-course-popup.component';
-import { course } from '../../models/course';
-import { AuthService } from '../../services/auth.service';
-import { CourseService } from '../../services/course.service';
+  import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+  import { MatDialog } from '@angular/material/dialog';
+  import { Router } from '@angular/router';
+  import { ToastService } from '../../Common/toast.service';
+  import { AddCoursePopupComponent } from '../../dialogues/add-course-popup/add-course-popup.component';
+  import { DeleteCoursePopupComponent } from '../../dialogues/delete-course-popup/delete-course-popup.component';
+  import { course } from '../../models/course';
+  import { AuthService } from '../../services/auth.service';
+  import { CourseService } from '../../services/course.service';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent {
-  errorLabel!: boolean;
-  successLabel!: boolean;
-  notifyMessage: any;
-  searchQuery = '';
-  isAdmin !: boolean;
-  courses!: course[];
-  currentPage = 1;
-  itemsPerPage = 5;
-  constructor(private authService: AuthService, private router: Router, private toast: ToastService,private courseService:CourseService,private dialog:MatDialog) { }
+  @Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+  })
+  export class HomeComponent {
+    errorLabel!: boolean;
+    successLabel!: boolean;
+    notifyMessage: any;
+    searchQuery = '';
+    isAdmin !: boolean;
+    courses!: course[];
+    currentPage = 1;
+    itemsPerPage = 5;
+    filterForm!: FormGroup;
+    constructor(private authService: AuthService, private router: Router, private toast: ToastService, private courseService: CourseService, private dialog: MatDialog, private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.getCourse();
-    this.isAdmin = this.authService.isAdmin();
-  }
+    ngOnInit(): void {
+      this.isAdmin = this.authService.isAdmin();
+      this.getCourse();
+      this.initilizeForm();
+    }
 
-  getCourse() {
-    this.toast.showLoader();
-    var res = this.courseService.getCourses().subscribe(
-      (response) => {
-        this.courses = response;
-        this.toast.stopLoader();
-      },
-      (error) => {
-        this.showError("An error occured. contact your system administrator.");
-        this.toast.stopLoader();
+    initilizeForm() {
+      this.filterForm = this.formBuilder.group({
+        technology: ['', Validators.required],
+        durationFrom: [null],
+        durationTo: [null],
+      }, {
+        validator: this.durationValidation
       });
-  }
+    }
 
-  openAddCourseDialog() {
-    const dialogRef = this.dialog.open(AddCoursePopupComponent, {
-      width:'500px'
-    });
+    durationValidation(group: AbstractControl): { [key: string]: boolean } | null {
+      const durationFrom = group.get('durationFrom')?.value;
+      const durationTo = group.get('durationTo')?.value;
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.refreshPagesAfterAdded();
-        this.showSuccess("Course Added Successfully");
+      if (durationFrom !== null && durationTo === null) {
+        return { 'durationToRequired': true };
       }
-      else {
-        this.toast.stopLoader();
-      }
-    });
-  }
 
-  refreshPagesAfterAdded() {
-    this.courseService.getCourses().subscribe(
-      (response) => {
-        this.courses = response;
-        var count = this.totalPages;
-        if (count == 1) {
-          this.currentPage = 1;
-        } else {
-          this.currentPage = count;
+      return null;
+    }
+
+    applyFilter() {
+      if (this.filterForm.valid) {
+        // Filter data based on the form values
+        console.log('Filtering data:', this.filterForm.value);
+        // Add your filtering logic here
+      } else {
+        console.log('Please fill out all fields with valid values.');
+      }
+    }
+
+    getCourse() {
+      this.toast.showLoader();
+      var res = this.courseService.getCourses().subscribe(
+        (response) => {
+          this.courses = response;
+          this.toast.stopLoader();
+        },
+        (error) => {
+          this.showError("An error occured. contact your system administrator.");
+          this.toast.stopLoader();
+        });
+    }
+
+    openAddCourseDialog() {
+      const dialogRef = this.dialog.open(AddCoursePopupComponent, {
+        width:'500px'
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.refreshPagesAfterAdded();
+          this.showSuccess("Course Added Successfully");
         }
-        this.toast.stopLoader();
-      },
-      (error) => {
-        this.showError("An error occured. contact your system administrator.");
-        this.toast.stopLoader();
+        else {
+          this.toast.stopLoader();
+        }
       });
+    }
+
+    refreshPagesAfterAdded() {
+      this.courseService.getCourses().subscribe(
+        (response) => {
+          this.courses = response;
+          var count = this.totalPages;
+          if (count == 1) {
+            this.currentPage = 1;
+          } else {
+            this.currentPage = count;
+          }
+          this.toast.stopLoader();
+        },
+        (error) => {
+          this.showError("An error occured. contact your system administrator.");
+          this.toast.stopLoader();
+        });
    
-  }
+    }
 
-  openDeleteCourseDialog(course: course) {
-    var tech = course.technology;
-    const dialogRef = this.dialog.open(DeleteCoursePopupComponent, {
-      width: '300px',
-      data: { tech }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.deleteCourse(tech);
-      }
-      else {
-        this.toast.stopLoader();
-      }
-    });
-  }
-
-  deleteCourse(courseName: string) {
-    this.courseService.deleteCourse(courseName).subscribe(
-      (response) => {
-        this.showSuccess(response.message);
-        this.refreshPagesAfterDeleted(courseName);
-        this.getCourse();
-      },
-      (error) => {
-        this.toast.stopLoader();
-        this.showError("An error occured. contact your system administrator.");
+    openDeleteCourseDialog(course: course) {
+      var tech = course.technology;
+      const dialogRef = this.dialog.open(DeleteCoursePopupComponent, {
+        width: '300px',
+        data: { tech }
       });
-  }
 
-  refreshPagesAfterDeleted(courseName:string) {
-    // Remove the deleted course from the courses array
-    this.courses = this.courses.filter(course => course.technology !== courseName);
-    // Reset the current page to the first page
-    var count = this.totalPages;
-    if (count == 1) {
-      this.currentPage = 1;
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.deleteCourse(tech);
+        }
+        else {
+          this.toast.stopLoader();
+        }
+      });
     }
-  }
 
-  logout() {
-    this.toast.showLoader();
-    this.authService.logout();
-    this.router.navigate(["login"]);
-    this.toast.showSuccess('Logout Successfully!');
-    this.toast.stopLoader();
-  }
+    deleteCourse(courseName: string) {
+      this.courseService.deleteCourse(courseName).subscribe(
+        (response) => {
+          this.showSuccess(response.message);
+          this.refreshPagesAfterDeleted(courseName);
+          this.getCourse();
+        },
+        (error) => {
+          this.toast.stopLoader();
+          this.showError("An error occured. contact your system administrator.");
+        });
+    }
+
+    refreshPagesAfterDeleted(courseName:string) {
+      // Remove the deleted course from the courses array
+      this.courses = this.courses.filter(course => course.technology !== courseName);
+      // Reset the current page to the first page
+      var count = this.totalPages;
+      if (count == 1) {
+        this.currentPage = 1;
+      }
+    }
+
+    logout() {
+      this.toast.showLoader();
+      this.authService.logout();
+      this.router.navigate(["login"]);
+      this.toast.showSuccess('Logout Successfully!');
+      this.toast.stopLoader();
+    }
    
-  closeNotify() {
-    this.errorLabel = false;
-    this.notifyMessage = null;
-    this.successLabel = false;
-  }
+    closeNotify() {
+      this.errorLabel = false;
+      this.notifyMessage = null;
+      this.successLabel = false;
+    }
 
-  showError(errMsg : string) {
-    this.errorLabel = true;
-    this.notifyMessage = errMsg;
-    setTimeout(() => {
-      this.closeNotify();
-    }, 8000);
-  }
+    showError(errMsg : string) {
+      this.errorLabel = true;
+      this.notifyMessage = errMsg;
+      setTimeout(() => {
+        this.closeNotify();
+      }, 8000);
+    }
 
-  showSuccess(errMsg: string) {
-    this.successLabel = true;
-    this.notifyMessage = errMsg;
-    setTimeout(() => {
-      this.closeNotify();
-    }, 8000);
-  }
+    showSuccess(errMsg: string) {
+      this.successLabel = true;
+      this.notifyMessage = errMsg;
+      setTimeout(() => {
+        this.closeNotify();
+      }, 8000);
+    }
 
-  get totalPages(): number {
-    return Math.ceil(this.courses.length / this.itemsPerPage);
-  }
+    get totalPages(): number {
+      return Math.ceil(this.courses.length / this.itemsPerPage);
+    }
 
-  // Calculate the start and end index for the current page
-  get startIndex(): number {
-    return (this.currentPage - 1) * this.itemsPerPage;
-  }
+    // Calculate the start and end index for the current page
+    get startIndex(): number {
+      return (this.currentPage - 1) * this.itemsPerPage;
+    }
 
-  get endIndex(): number {
-    return this.currentPage * this.itemsPerPage;
-  }
+    get endIndex(): number {
+      return this.currentPage * this.itemsPerPage;
+    }
 
-  // Function to navigate to the previous page
-  previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
+    // Function to navigate to the previous page
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    }
+
+    // Function to navigate to the next page
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
     }
   }
-
-  // Function to navigate to the next page
-  nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-    }
-  }
-}
